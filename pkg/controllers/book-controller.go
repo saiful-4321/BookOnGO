80a983073341543a1aa97d072d5163186de59f1b
Md. Saiful Islam <bd.saif321@gmail.com>
2023-01-04 03:26:33 +0600

    Getting all books process added on controller

--- pkg/controllers/book-controller.go
+++ pkg/controllers/book-controller.go
@@ -1,1 +1,19 @@
 package controllers
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/saiful-4321/crud/pkg/models"
+)
+
+var newBook models.Book
+
+func getBooks(w http.ResponseWriter, r *http.Request) {
+	newBooks := models.GetAllBooks()
+
+	response, _ := json.Marshal((newBooks))
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(response)
+}
