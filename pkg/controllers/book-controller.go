2715ea4bf67bae6df24d2507f101d65ff48b8be1
saiful-4321 <bd.saif321@gmail.com>
2023-01-04 22:09:01 +0600

    created controller method to get item by id

--- pkg/controllers/book-controller.go
+++ pkg/controllers/book-controller.go
@@ -2,18 +2,38 @@ package controllers
 
 import (
 	"encoding/json"
+	"fmt"
 	"net/http"
+	"strconv"
 
+	"github.com/gorilla/mux"
 	"github.com/saiful-4321/crud/pkg/models"
 )
 
 var newBook models.Book
 
-func getBooks(w http.ResponseWriter, r *http.Request) {
+func GetBooks(w http.ResponseWriter, r *http.Request) {
 	newBooks := models.GetAllBooks()
 
 	response, _ := json.Marshal((newBooks))
-	w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("Content-Type", "pkglication/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(response)
+}
+
+func GetBookById(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	bookId := vars["bookId"]
+	ID, err := strconv.ParseInt(bookId, 0, 0)
+
+	if err != nil {
+		fmt.Println("Error while parsing")
+	}
+
+	bookDetails, _ := models.GetBookById(ID)
+	response, _ := json.Marshal(bookDetails)
+
+	w.Header().Set("Content-Type", "pkglication/json")
 	w.WriteHeader(http.StatusOK)
 	w.Write(response)
 }
--- pkg/routes/routes.go
+++ pkg/routes/routes.go
@@ -7,7 +7,7 @@ import (
 
 var RegisterBookStores = func(router *mux.Router) {
 	router.HandleFunc("/book", controllers.CreateBook).Methods("POST")
-	router.HandleFunc("/book", controllers.GetBook).Methods("GET")
+	router.HandleFunc("/book", controllers.GetBooks).Methods("GET")
 	router.HandleFunc("/book/{bookId}", controllers.GetBookById).Methods("GET")
 	router.HandleFunc("/book/{bookId}", controllers.UpdateBook).Methods("PUT")
 	router.HandleFunc("/book/{bookId}", controllers.DeleteBook).Methods("DELETE")
