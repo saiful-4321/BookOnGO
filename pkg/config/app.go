22b0feedd2d93cc8562df3bac266e702d8155e03
saiful-4321 <bd.saif321@gmail.com>
2023-01-05 01:57:06 +0600

    bug fixed and postman collection added in repo

--- pkg/config/app.go
+++ pkg/config/app.go
@@ -10,7 +10,7 @@ var (
 )
 
 func Connect() {
-	d, err := gorm.Open("mysql", "root:S@if1719/simplerest?charset=utf8&parseTime=True&loc=Local")
+	d, err := gorm.Open("mysql", "root:S@if1719@tcp(127.0.0.1:3306)/simplerest?charset=utf8&parseTime=True&loc=Local")
 
 	if err != nil {
 		panic(err)
